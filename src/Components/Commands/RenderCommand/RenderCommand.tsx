feebf1a7d9c670d3c7dc023201a4e3f26a0af636
parwarr <parwar.habasch@gmx.de>
2024-02-02 22:26:39 +0100

    Revert "feat: update font size"
    This reverts commit ab8e7a90b8e3ea51ca20872e0c732feeaa8a2344.

--- src/Components/Commands/RenderCommand/RenderCommand.tsx
+++ src/Components/Commands/RenderCommand/RenderCommand.tsx
@@ -11,13 +11,23 @@ const RenderCommand = ({ commands }: CommandProp) => {
       <div className='container '>
         <div className='flex flex-col items-center justify-center'>
           <div className='flex flex-col items-center justify-center'>
-            {commands.map((command, index) => (
-              <div key={index} className='flex flex-row items-center justify-center mb-3'>
-                <p className='text-white text-lg font-bold'>{command.command}</p>
-                <p className='text-white text-lg font-bold px-2'>{'-'}</p>
-                <p className='text-white text-lg font-bold'>{command.description}</p>
-              </div>
-            ))}
+            <p className='text-white text-2xl font-bold'>Skills</p>
+            <div className='flex flex-col items-center justify-center'>
+              {commands.map((command, index) => (
+                <div
+                  key={index}
+                  className='flex flex-row items-center justify-center mb-3'
+                >
+                  <p className='text-white text-2xl font-bold'>
+                    {command.command}
+                  </p>
+                  <p className='text-white text-2xl font-bold px-2'>{'-'}</p>
+                  <p className='text-white text-2xl font-bold'>
+                    {command.description}
+                  </p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>

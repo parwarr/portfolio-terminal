5adcdf4cd25a05ccc4124fa592dc9160359762fd
parwarr <parwar.habasch@gmx.de>
2024-02-03 14:09:51 +0100

    Revert "Revert "feat: update font size""
    This reverts commit feebf1a7d9c670d3c7dc023201a4e3f26a0af636.

--- src/Components/Commands/RenderCommand/RenderCommand.tsx
+++ src/Components/Commands/RenderCommand/RenderCommand.tsx
@@ -1,19 +1,29 @@
-import { TypingEffect } from '../../TypingEffect/TypingEffect';
-
 interface CommandProp {
-  commands: string[];
+  commands: {
+    command: string;
+    description: string;
+  }[];
 }
-
 const RenderCommand = ({ commands }: CommandProp) => {
   return (
-    <div className='flex flex-col items-start justify-start'>
-      {commands.map((command, index) => (
-        <div key={index} className='flex flex-row items-start justify-start mb-3'>
-          <TypingEffect text={command} speed={50} />
+    <>
+      <div className='container '>
+        <div className='flex flex-col items-center justify-center'>
+          <div className='flex flex-col items-center justify-center'>
+            <p className='text-white text-2xl font-bold'>Skills</p>
+            <div className='flex flex-col items-center justify-center'>
+              {commands.map((command, index) => (
+                <div key={index} className='flex flex-row items-center justify-center mb-3'>
+                  <p className='text-white text-2xl font-bold'>{command.command}</p>
+                  <p className='text-white text-2xl font-bold px-2'>{'-'}</p>
+                  <p className='text-white text-2xl font-bold'>{command.description}</p>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-      ))}
-    </div>
+      </div>
+    </>
   );
 };
-
 export default RenderCommand;

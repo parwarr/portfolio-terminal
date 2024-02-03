660fd2a2c598d80230dcb72746412b75f4ae0392
parwarr <parwar.habasch@gmx.de>
2024-02-03 16:57:36 +0100

    added banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Banner from './Components/Banner/Banner';
 import InputField from './Components/InputField/InputField';
 
 function App() {
   return (
     <>
+      <Banner />
       <InputField />
     </>
   );

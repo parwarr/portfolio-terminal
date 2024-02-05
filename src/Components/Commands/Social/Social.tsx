ff75452fc068c2135cd2e58105d7e6595cfcb8c8
Habasch Parwar, GHR-POD-NEX-18 <Parwar.Habasch@swisscom.com>
2024-02-05 17:03:04 +0100

    update

--- src/Components/Commands/Social/Social.tsx
+++ src/Components/Commands/Social/Social.tsx
@@ -4,8 +4,8 @@ const Socials = () => {
   const socialsCommand = [
     {
       command: 'LinkedIn',
-      description: 'www.linkedin.com/in/parwar-h-6a48a226b',
-      url: 'www.linkedin.com/in/parwar-h-6a48a226b',
+      description: 'https://www.linkedin.com/in/parwar-h-6a48a226b/',
+      url: 'https://www.linkedin.com/in/parwar-h-6a48a226b/',
     },
     {
       command: 'Github',

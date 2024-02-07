b367be08ab0c80808c8d8eb00a55b06cee08a12f
parwarr <parwar.habasch@gmx.de>
2024-02-07 23:42:30 +0100

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import InputField from './Components/InputField/InputField';
 
 function App() {
   return (
     <>
-      <InputField />
+      <Routes>
+        <Route index path='/' element={<InputField />} />
+        <Route path='gui' element={<div>GUI</div>} />
+      </Routes>
     </>
   );
 }
---
+++ src/Components/Gui/Gui.tsx
@@ -0,0 +1,9 @@
+const Gui = () => {
+  return (
+    <div>
+      <h1>Gui</h1>
+    </div>
+  );
+};
+
+export default Gui;
